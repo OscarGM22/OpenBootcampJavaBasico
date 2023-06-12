bf953d3e4a2d9cad497187ab9daf0870b2874354
OscarGM22 <osgimu@gmail.com>
2023-06-12 20:07:29 +0200

    Eliminados prints con ".toString" y modificados para mejor legibilidad.

--- src/Tema4/ClasesObjetosHerenciasYPolimorfismos/Main.java
+++ src/Tema4/ClasesObjetosHerenciasYPolimorfismos/Main.java
@@ -6,16 +6,16 @@ public class Main {
         // SmartDevice device = new SmartDevice();  --> No se puede instanciar porque la clase SmartDevice es abstracta.
 
         SmartPhone miSmartPhone = new SmartPhone(); // Usamos el constructor vacío.
-        System.out.println(miSmartPhone.toString()); // Al no darle parámetros y los atributos ser tipo envoltorio (con mayúscula e int-->Integer), imprime 'null'.
+        // System.out.println(miSmartPhone); // Al no darle parámetros y los atributos ser tipo envoltorio (con mayúscula e int-->Integer), imprime 'null' como valor de todos ellos.
 
         SmartPhone miIPhone = new SmartPhone("Apple", "iPhone", 2942, 120, true,6.2,true,64);
-        System.out.println(miIPhone.toString());
+        System.out.println("El SmartPhone es: " + "\n" + miIPhone);
 
         SmartWatch miWatch = new SmartWatch(); // Usamos el constructor vacío.
-        System.out.println(miWatch.toString());
+        // System.out.println(miWatch);
 
         SmartWatch miAmazfit = new SmartWatch("Amazfit", "Bip U Pro", 230, 40, true, true, true, 1, "Cuadrada");
-        System.out.println(miAmazfit.toString());
+        System.out.println("El SmartWatch es: " + "\n" + miAmazfit);
 
 
 
